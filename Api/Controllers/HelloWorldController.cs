66db9f05029eef7b97bbdb660f041b2b7ed3bbbc
Thiago Mathias <thiago.santos199@fatec.sp.gov.br>
2022-03-31 20:24:13 -0300

    add MultipleActiveResultSets

---
+++ Api/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConstruFindAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
